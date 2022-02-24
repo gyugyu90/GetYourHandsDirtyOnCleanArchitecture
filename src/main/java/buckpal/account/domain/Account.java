a24ed19897214761c2d800900051723dcccf778c
gyugyu90 <kh2000park@gmail.com>
2022-02-24 18:26:21 +0900

    feat(Account): add constructor

--- src/main/java/buckpal/account/domain/Account.java
+++ src/main/java/buckpal/account/domain/Account.java
@@ -2,15 +2,26 @@ package buckpal.account.domain;
 
 import java.time.LocalDateTime;
 
+import lombok.AccessLevel;
+import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.Value;
 
 @Data
+@AllArgsConstructor(access = AccessLevel.PRIVATE)
 public class Account {
 
 	private AccountId id;
 	private Money baselineBalance;
 	private ActivityWindow activityWindow;
+
+	public static Account withoutId(Money baselineBalance, ActivityWindow activityWindow) {
+		return new Account(null, baselineBalance, activityWindow);
+	}
+
+	public static Account withId(AccountId accountId, Money baselineBalance, ActivityWindow activityWindow) {
+		return new Account(accountId, baselineBalance, activityWindow);
+	}
 
 	public Money calculateBalance() {
 		return Money.add(
